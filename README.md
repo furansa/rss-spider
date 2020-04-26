c5ce83b40710eee8f4e4baddd4bb39eef23875b8
Fernando França (フランサ) <github@furansa.me>
2020-04-26 11:27:34 -0300

    Started to work on fetch and parse function.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # RSS Spider
-A very simple RSS spider for late reading. Can be used for terminal reading
-or integrated with tools like [Conky monitor](https://github.com/brndnmtthws/conky).
+A very simple RSS spider using nothing but default modules. Can be used for
+terminal reading or integrated with tools like
+[Conky monitor](https://github.com/brndnmtthws/conky).
 
 ## History:
 * 0.0.1, 2020-04-25: Very first version.
--- rss_spider.py
+++ rss_spider.py
@@ -3,22 +3,60 @@ import argparse
 import json
 import os
 import sys
+from urllib import error, request
+from xml.etree.ElementTree import parse
 
 
 def create_default_source(filename: str) -> None:
-    """Create RSS feed source examples into given file."""
+    """
+    Create RSS feed source examples into given file.
+    """
     feeds = {
-        "My Feed": "http://myfeed.com/rss",
-        "Another Feed": "http://anotherfeed.com/rss",
-        "Third Feed": "http://thirdfeed.com/rss",
+        "Planet Python": "https://planetpython.org/rss20.xml"
     }
 
     with open(filename, "w") as file:
         json.dump(feeds, file)
 
 
+def fetch_parse_feeds(source: str, destination: str, limit: int) -> None:
+    """
+    Fetch RSS feed source and parse the content into destination according to
+    the given limit of days.
+    """
+    request_headers = {"User-Agent": "Mozilla/5.0"}
+
+    with open(source, "r") as file:
+        rss_sources = json.load(file)
+
+    for title, url in rss_sources.items():
+        print("Accessing " + title)
+
+        rss_request = request.Request(url, headers=request_headers)
+
+        try:
+            with request.urlopen(rss_request) as response:
+                rss_doc = parse(response)
+
+                for item in rss_doc.iterfind("channel/item"):
+                    print("Title: " + item.findtext("title"))
+                    print("Link: " + item.findtext("link") + "\n")
+
+        except error.URLError as e:
+            print(e.reason)
+
+
+def read_robots() -> None:
+    """
+    Read from RSS feed robots.txt
+    """
+    pass
+
+
 if __name__ == "__main__":
-    """Main program"""
+    """
+    Main program entrypoint
+    """
     # Command line parser
     arg_parser = argparse.ArgumentParser()
 
@@ -30,12 +68,13 @@ if __name__ == "__main__":
 
     arg_parser.add_argument("-d", "--destination", dest="destination",
                             action="store", required=False, default="/tmp/rss",
-                            help="Destination path, a default /tmp/rss will be \
-                            created if not informed")
+                            help="Destination directory, a default /tmp/rss \
+                            will be created if not informed")
 
-    arg_parser.add_argument("-l", "--limit", dest="limit", action="store",
-                            required=False, default="1", help="Limit the number \
-                            of days to download. Default 1 (today)")
+    arg_parser.add_argument("-l", "--limit", type=int, dest="limit",
+                            action="store", required=False, default="1",
+                            help="Limit the number of days to download. \
+                            Default 1 (today)")
 
     args = arg_parser.parse_args()
 
@@ -49,11 +88,15 @@ if __name__ == "__main__":
 
         sys.exit()
 
-    # Create destination path if the informed one does not exits
+    # Create destination directory if the informed one does not exits
     if not os.path.isdir(args.destination):
-        print("Informed destination path " + args.destination +
+        print("Informed destination directory " + args.destination +
               " does not exists, creating.")
 
         os.mkdir(args.destination)
+
+    # Fetch and parse from RSS feed sources based on the given limit of days
+    # and save to destination directory
+    fetch_parse_feeds(args.source, args.destination, args.limit)
 
     sys.exit()
